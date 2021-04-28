c88877766c225aa2ae2cacd596604fc0229edd5a
Bryan Buchanan <bryan@resen.co>
2021-04-27 19:00:27 -0700

    Setup

--- src/scripts.js
+++ src/scripts.js
@@ -1,111 +1,13 @@
-import {EditorState} from "prosemirror-state"; // Required
+import {EditorState, Plugin} from "prosemirror-state"; // Required
 import {EditorView} from "prosemirror-view"; // Required
 import {Schema, DOMParser, DOMSerializer} from "prosemirror-model"; // Required
 
-import {Plugin} from "prosemirror-state";
+import {schema} from "prosemirror-schema-basic";
 import {toggleMark, setBlockType, wrapIn, baseKeymap} from "prosemirror-commands";
 import {keymap} from "prosemirror-keymap";
 
 import {undo, redo, history} from "prosemirror-history";
 import {wrapInList, splitListItem, liftListItem, sinkListItem} from "prosemirror-schema-list";
-
-
-let menu = menuPlugin([
-	{
-		command: toggleMark(schema.marks.strong),
-		dom: button('<i class="fas fa-bold"></i>', 'bold')
-	},
-	{
-		command: toggleMark(schema.marks.em),
-		dom: button('<i class="fal fa-italic"></i>', "italic")
-	},
-	{
-		command: setBlockType(schema.nodes.heading, {level: 2 }),
-		dom: button("H2", "heading1")
-	},
-	{
-		command: setBlockType(schema.nodes.heading, {level: 3 }),
-		dom: button("H3", "heading")
-	},
-	{
-		command: wrapIn(schema.nodes.blockquote),
-		dom: button(">", "blockquote")
-	}
-]);
-
-let state = EditorState.create({
-	doc: DOMParser.fromSchema(schema).parse(content),
-	// plugins: exampleSetup({ schema: schema })
-	plugins: [
-		keymap({
-			"Mod-z": undo,
-			"Mod-y": redo,
-			"Mod-b": toggleMark(schema.marks.strong),
-			"Mod-i": toggleMark(schema.marks.em),
-			"Shift-Enter": schema.nodes.hard_break.create()
-		}),
-		keymap(baseKeymap),
-		history(),
-		menu
-	]
-});
-
-let view = new EditorView(editor, {
-	state,
-	dispatchTransaction(transaction) {
-		
-		// Update editor state
-		let previousState = view.state.doc;
-		let newState = view.state.apply(transaction);
-		view.updateState(newState);
-		
-		// Save content
-		if (!previousState.eq(view.state.doc)) {
-			tta.text.save(transaction, schema, view);
-		}
-		
-		let menu = view.dom.closest('.js-editor').querySelector('.js-textmenu');
-		
-		if (view.state.selection.empty) {
-			menu.classList.remove('active');
-		} else {
-			menu.classList.add('active');
-		}
-		
-		let {from, to} = view.state.selection;
-		let start = view.coordsAtPos(from), end = view.coordsAtPos(to);
-		
-		if (menu.offsetParent) {
-		
-			let box = menu.offsetParent.getBoundingClientRect();
-			let left = Math.max((start.left + end.left) / 2, start.left + 3);
-			left = left - (menu.offsetWidth / 2);
-						
-			menu.style.left = left + "px";
-			menu.style.bottom = (box.bottom - start.top + 10) + "px";
-			
-		}
-		
-	},
-	handleDOMEvents: {
-		focus: (view, event) => {
-			let container = event.target.closest('.js-editor');
-			this.wasFocused = true;
-			container.classList.add('focused');
-			let id = event.target.closest('section').dataset.id;
-			tta.widgets.edit(id);
-			return false;
-		},
-		blur: (view, event) => {
-			if (this.wasFocused) {
-				let container = event.target.closest('.js-editor');
-				container.classList.remove('focused');
-				container.querySelector('.js-textmenu').classList.remove('active');
-			}
-			return false;
-		}
-	}
-});
 
 class MenuView {
 	
@@ -116,11 +18,25 @@ class MenuView {
 		
 		// Get menu template from template bank
 		let container = document.createElement('div');
-		container.innerHTML = document.getElementById('template-textmenu').innerHTML;
-		this.dom = container.querySelector('*');
+		container.innerHTML = `<div class="textmenu js-textmenu">
+		
+		<div title="Bold" class="textmenu__button textmenu__button--bold js-bold"><i class="fas fa-bold"></i></div>
+		<div title="Italic" class="textmenu__button textmenu__button--italic js-italic"><i class="fal fa-italic"></i></div>
+		<div title="Link" class="textmenu__button textmenu__button--link js-link"><i class="fal fa-link"></i></div>
+		
+		<div class="textmenu__divider"></div>
+		
+		<div title="Heading 1" class="textmenu__button textmenu__button--heading1 js-heading1"><i class="fal fa-heading"></i></div>
+		<div title="Heading 2" class="textmenu__button textmenu__button--heading2 js-heading2"><i class="fal fa-heading"></i></div>
+		
+		<div class="textmenu__divider"></div>
+		
+		<div title="Bullet List" class="textmenu__button textmenu__button--list js-list"><i class="fal fa-list"></i></div>
+		<div title="Numbered List" class="textmenu__button textmenu__button--numberedlist js-numberedlist"><i class="fal fa-list-ol"></i></div>
+		<div title="Quote" class="textmenu__button textmenu__button--quote js-quote"><i class="fal fa-quote-left"></i></div>
 	
-		// Update for some reason
-		this.update();
+	</div>`;
+		this.dom = container.querySelector('*');
 	
 		this.dom.addEventListener('mousedown', e => {
 	
@@ -195,19 +111,108 @@ class MenuView {
 		});
 	}
 	
-	update() {
-		this.items.forEach(({command, dom}) => {
-			let active = command(this.editorView.state, null, this.editorView);
-			if (dom.style.display = active) {
-				dom.classList.remove('actove');
-			} else {
-				dom.classList.add('active');
-			}
-		});
-	}
+	// update() {
+	// 	this.items.forEach(({command, dom}) => {
+	// 		let active = command(this.editorView.state, null, this.editorView);
+	// 		if (dom.style.display = active) {
+	// 			dom.classList.remove('actove');
+	// 		} else {
+	// 			dom.classList.add('active');
+	// 		}
+	// 	});
+	// }
 	
 	destroy() { this.dom.remove(); }
 	
+}
+
+// Get all elements that should contain an editor
+let texts = document.querySelectorAll('.js-text');
+
+// Loop through each one
+for (let text of texts) {
+		
+	let content = text.querySelector('.js-content');
+	let editor = text.querySelector('.js-editor');
+	
+	let menu = menuPlugin();
+	
+	let state = EditorState.create({
+		doc: DOMParser.fromSchema(schema).parse(content),
+		// plugins: exampleSetup({ schema: schema })
+		plugins: [
+			keymap({
+				"Mod-z": undo,
+				"Mod-y": redo,
+				"Mod-b": toggleMark(schema.marks.strong),
+				"Mod-i": toggleMark(schema.marks.em),
+				"Shift-Enter": schema.nodes.hard_break.create()
+			}),
+			keymap(baseKeymap),
+			history(),
+			menu
+		]
+	});
+	
+	let view = new EditorView(editor, {
+		state,
+		dispatchTransaction(transaction) {
+			
+			// Update editor state
+			let previousState = view.state.doc;
+			let newState = view.state.apply(transaction);
+			view.updateState(newState);
+			
+			// Save content
+			if (!previousState.eq(view.state.doc)) { }
+			
+			let menu = view.dom.closest('.js-editor').querySelector('.js-textmenu');
+			
+			if (view.state.selection.empty) {
+				menu.classList.remove('active');
+			} else {
+				menu.classList.add('active');
+			}
+			
+			let {from, to} = view.state.selection;
+			let start = view.coordsAtPos(from), end = view.coordsAtPos(to);
+			
+			if (menu.offsetParent) {
+				
+				console.log(start.left, end.left);
+			
+				let box = menu.offsetParent.getBoundingClientRect();
+				// let left = Math.max((start.left + end.left) / 2, start.left + 3);
+				let left = (start.left + end.left) / 2;
+				// left = left - (menu.offsetWidth / 2);
+							
+				menu.style.left = left + "px";
+				menu.style.top = start.top - 60 + "px";
+				// menu.style.bottom = (box.bottom - start.top + 10) + "px";
+				
+			}
+			
+		},
+		handleDOMEvents: {
+			focus: (view, event) => {
+				let container = event.target.closest('.js-editor');
+				view.wasFocused = true;
+				container.classList.add('focused');
+				// let id = event.target.closest('section').dataset.id;
+				// tta.widgets.edit(id);
+				return false;
+			},
+			blur: (view, event) => {
+				if (view.wasFocused) {
+					let container = event.target.closest('.js-editor');
+					container.classList.remove('focused');
+					container.querySelector('.js-textmenu').classList.remove('active');
+				}
+				return false;
+			}
+		}
+	});
+
 }
 
 function menuPlugin(items) {
@@ -219,3 +224,9 @@ function menuPlugin(items) {
 		}
 	})
 }
+
+
+
+
+
+
